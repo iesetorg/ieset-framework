b398e7e5d6da93e5e2049d9a775305671646034d
iesetorg <iesetorg@users.noreply.github.com>
2026-05-04 10:52:09 +0400

    Retire old EFW panel artifacts

--- engine/runs/efw_market_panel_efw_summary_fdi_inflows_share_20260503/replication.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Replicate this panel run from the preregistered hypothesis spec."""
-from scripts.run_panel_fe import run_one
-
-if __name__ == '__main__':
-    print(run_one('efw_market_panel_efw_summary_fdi_inflows_share_20260503', force=True, persist_preflight_inconclusive=True))
--- engine/runs/efw_oecd_panel_property_rights_inflation_rate_20260503/replication.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-"""Replicate this panel run from the preregistered hypothesis spec."""
-from scripts.run_panel_fe import run_one
-
-if __name__ == '__main__':
-    print(run_one('efw_oecd_panel_property_rights_inflation_rate_20260503', force=True, persist_preflight_inconclusive=True))
